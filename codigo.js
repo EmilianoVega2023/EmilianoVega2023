6a4f759c4382c0d55f3db13d4a11019a8b6e4bb1
EmilianoVega2023 <vemiliano220@gmail.com>
2024-01-15 01:13:26 -0300

    Ejerciciorotar

--- codigo.js
+++ codigo.js
@@ -1,11 +1,31 @@
-// Se quiere tener un contador de dos dígitos en base 5. El funcionamiento es simple: el dígito de la derecha irá aumentando y cuando pase de 4 su valor se pondrá a 0 y el dígito de la izquierda se incrementa en 1. El contador debe deternerse cuando alcancemos el valor 1:4
-let unidades = 0;
-let decenas = 0;
-for(i = 0; i = 14;i++){
-    unidades = unidades + 1;
-    if(unidades = 9){
-        decenas = decenas + 1;
-        unidades = 0;
-    }
-    console.log(`${decenas} : ${unidades}`);
+//En este ejercicio debes crear dos arrays para representar dos equipos de trabajo. Al primer array le llamaremos ocupados y al segundo libres. Ambos están llenos con 5 nombres.
+
+// Debes crear una función rotar(lst1, lst2) que pase el primer nombre de la lista lst1 al final de lst2, y luego el primero de la lst2 al final de lst1.
+
+// Para ver que funciona escribe los arrays antes y después de usar esta función.
+
+// let lst1 = ["juan","pedro","emilinao","kaka","ramon"];
+// let lst2 = ["adrian","carlos","rodrigo","ulises","rayo"];
+
+// function rotar(lst1,lst2){
+//    var element = lst1.shift();
+//    lst2[5] = element;
+//    var element2 = lst2.shift();
+//    lst1[4] = element2;
+//    console.log(lst1,lst2);
+// }
+// rotar(lst1,lst2);
+
+
+function rotar(lst1, lst2){
+    let persona;
+   persona = lst1.shift();
+   lst2.push(persona);
+   persona= lst2.shift();
+   lst1.push(persona);
 }
+let ocupados = ['Pedro', 'Luisa', 'Juan', 'Ana'];
+let libres = ['Marina', 'Jose', 'Alba', 'Felipe'];
+console.log("Inicio ", ocupados, libres);
+rotar(ocupados, libres);
+console.log("Final ", ocupados, libres);
