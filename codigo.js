6d72a785e2dc5811e42b633f5311d5cdc259aeba
EmilianoVega2023 <vemiliano220@gmail.com>
2024-01-15 14:54:28 -0300

    valoresPares

--- codigo.js
+++ codigo.js
@@ -1,11 +1,27 @@
 //Mostrar todos los números de 1 a N aumentando de 2 en 2 donde n lo ingresa el usuario en un prompt.
-const usuario = parseInt(prompt("Digite su numero: "));
-let array2 = [];
+const N = parseInt(prompt("Digite su numero: "));
+let valoresPares = [];
+let datos = [];
 
-
-for(let i = 0;i < usuario;i++){
-    if (i % 2 == 0){
-        let num = array2[i] = i + 1;
-        console.log(`${num} `);
-        }
+function ejecucion(){
+    
+for(let i = 0;i > N;i+2){
+    i = datos[i];
+    if (datos[i] == undefined) {
+        break;
+        } else{
+            valoresPares.push(datos[i]);
+            }
 }
+}
+
+ejecucion();
+    
+console.log(datos);
+
+// if (i % 2 == 0){
+//     const element = array2.push(i);
+//     console.log(`${element}`)
+//     console.log(i);
+//     }
+// }
