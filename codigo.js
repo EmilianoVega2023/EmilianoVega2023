6153f092fe35651caa704e12444b0e4b5a14286e
EmilianoVega2023 <vemiliano220@gmail.com>
2024-01-15 22:15:11 -0300

    valoresPares

--- codigo.js
+++ codigo.js
@@ -1,6 +1,11 @@
 // Crear una función que reciba un valor cualquiera y lo muestre ocupando console.log(), llamar a la función pasando el valor 5
 
+
+//Llenar un array vacio de 0  a n veces:
+//Valores pares: 
+
 let datos = [];
+let valoresPares = [];
 let n = prompt("Ingrese el valor de n:");
 
 n = parseInt(n);
@@ -13,3 +18,10 @@ if (isNaN(n) || n < 0) {
   }
   console.log("Array 'datos':", datos);
 }
+const elemet1 = datos.length();
+for(i=0;i => elemet1;i++){
+    if (datos[i] % 2 == 0) {
+      valoresPares.push(datos[i]);
+      }
+}
+console.log("Valores Pares: ",valoresPares)
